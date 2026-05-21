c49f86ef86e9881225f7e62dbddbb30b21f28418
odolpez <odolpez@gmail.com>
2026-05-21 15:57:11 +0200

    Remove /chats and /explanations routes and delete replaced list page files

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import StartPage from './pages/StartPage';
-import ChatsListPage from './pages/ChatsListPage';
 import ChatViewPage from './pages/ChatViewPage';
 import SettingsPage from './pages/SettingsPage';
-import SavedExplanationsListPage from './pages/SavedExplanationsListPage';
 import ExplanationViewPage from './pages/ExplanationViewPage';
 
 function App() {
@@ -11,10 +9,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<StartPage />} />
-        <Route path="/chats" element={<ChatsListPage />} />
         <Route path="/chat/:id" element={<ChatViewPage />} />
         <Route path="/settings" element={<SettingsPage />} />
-        <Route path="/explanations" element={<SavedExplanationsListPage />} />
         <Route path="/explanation/:id" element={<ExplanationViewPage />} />
       </Routes>
     </BrowserRouter>
