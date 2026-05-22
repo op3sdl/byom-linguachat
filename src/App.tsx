3bf6c644a3867a0d43889ae48725c6dd150d72a0
odolpez <odolpez@gmail.com>
2026-05-22 12:41:51 +0200

    Remove ExplanationViewPage and its route from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import StartPage from './pages/StartPage';
 import ChatViewPage from './pages/ChatViewPage';
 import SettingsPage from './pages/SettingsPage';
-import ExplanationViewPage from './pages/ExplanationViewPage';
 
 function App() {
   return (
@@ -11,7 +10,6 @@ function App() {
         <Route path="/" element={<StartPage />} />
         <Route path="/chat/:id" element={<ChatViewPage />} />
         <Route path="/settings" element={<SettingsPage />} />
-        <Route path="/explanation/:id" element={<ExplanationViewPage />} />
       </Routes>
     </BrowserRouter>
   );
